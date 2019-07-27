e34a2d4e253bc2ad83ef1b7514b61ca34dcc9307
Mohan Prasath <openmohan@gmail.com>
2019-07-27 16:09:26 +0530

    gh pages

--- precache-manifest.01f0746ac22c52366fb229f99f89816a.js
+++ precache-manifest.01f0746ac22c52366fb229f99f89816a.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fc6b6f12e270c59b349ce91f0c4b3975",
-    "url": "/index.html"
+    "url": "https://raw.githubusercontent.com/openmohan/react-test-zion/gh-pages/index.html"
   },
   {
     "revision": "c0567a6cdd83a59dedbe",
-    "url": "/static/css/main.dd1bc90b.chunk.css"
+    "url": "https://raw.githubusercontent.com/openmohan/react-test-zion/gh-pages/static/css/main.dd1bc90b.chunk.css"
   },
   {
     "revision": "083b39e8465089e420d7",
-    "url": "/static/js/2.6d71cd5e.chunk.js"
+    "url": "https://raw.githubusercontent.com/openmohan/react-test-zion/gh-pages/static/js/2.6d71cd5e.chunk.js"
   },
   {
     "revision": "c0567a6cdd83a59dedbe",
-    "url": "/static/js/main.222c401f.chunk.js"
+    "url": "https://raw.githubusercontent.com/openmohan/react-test-zion/gh-pages/static/js/main.222c401f.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "https://raw.githubusercontent.com/openmohan/react-test-zion/gh-pages/static/js/runtime~main.a8a9905a.js"
   }
 ]);
